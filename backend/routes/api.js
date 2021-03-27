2e0c975dde124def85c6decfebfabf64c1b62a1b
djarosz-hub <jaroszdamian9@gmail.com>
2021-03-27 15:26:29 +0100

    api nearly finished

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,89 +7,146 @@ const db = mysql.createConnection(dbConfig);
 
 //authorize section
 router.post("/register", (req, res) => {
-  const login = req.body.login;
-  const password = req.body.password;
-  db.query("SELECT * FROM users WHERE login = ?", [login], (err, result) => {
-    if (err) {
-      res.send({ error: err });
-      return;
-    }
-    if (result.length > 0) {
-      res.send({ message: "User already exists!" });
-      return;
-    }
+  const { login, password } = req.body;
+  try {
+    db.query("SELECT * FROM users WHERE login = ?", [login], (err, result) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      if (result.length > 0) {
+        return res.status(200).json({ message: "User already exists!" });
+      }
+      db.query(
+        "INSERT INTO users (login,password) VALUES (?,?)",
+        [login, password],
+        (err, result) => {
+          if (err) {
+            return res.status(400).json({ error: err });
+          }
+          return res.status(201).json({ message: "Successfully created user" });
+        }
+      );
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+router.post("/login", (req, res) => {
+  const { login, password } = req.body;
+  try {
     db.query(
-      "INSERT INTO users (login,password) VALUES (?,?)",
+      "SELECT * FROM users WHERE login = ? AND password = ?",
       [login, password],
       (err, result) => {
         if (err) {
           res.send({ error: err });
+          return;
         }
-        res.send({ success: true });
+        if (result.length > 0) {
+          // res.send(result);
+          return res.status(200).json(result);
+          // return;
+        }
+        return res
+          .status(200)
+          .json({ message: "Wrong credentials or user doesn't exists!" });
       }
     );
-  });
-});
-router.post("/login", (req, res) => {
-  const login = req.body.login;
-  const password = req.body.password;
-
-  db.query(
-    "SELECT * FROM users WHERE login = ? AND password = ?",
-    [login, password],
-    (err, result) => {
-      if (err) {
-        res.send({ error: err });
-        return;
-      }
-      if (result.length > 0) {
-        res.send(result);
-        return;
-      }
-      res.send({ message: "Wrong credentials or user doesn't exists!" });
-    }
-  );
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
 
 //utils section
-router.get("/notes",(req,res)=>{
+router.get("/notes", (req, res) => {
   const login = req.query.login;
+  try {
+    db.query("SELECT * FROM notes WHERE owner = ?", [login], (err, result) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      if (result.length == 0) {
+        return res.status(200).json({ message: "no notes yet" });
+      }
+      res.send(result);
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+router.get("/notes/:id", (req, res) => {
+  const { nId, login } = req.query;
+  try {
     db.query(
-        "SELECT * FROM notes WHERE owner = ?", [login],(err,result)=>{
-            if(err){
-                res.send({message:"something went wrong"});
-                return;
-            }
-            if(result.length == 0){
-                res.send({message:"no notes yet"});
-                return;
-            }
-            res.send(result);
+      "SELECT * FROM notes WHERE id = ? AND owner = ?",
+      [nId, login],
+      (err, result) => {
+        if (err) {
+          return res.status(400).json({ error: err });
         }
+        return res.status(200).json(result);
+      }
     );
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
-router.get("/notes/:id",(req,res)=>{
-    const noteId = req.query.id;
+
+router.post("/notes", (req, res) => {
+  const { title, body, owner } = req.body;
+  try {
     db.query(
-        "SELECT * FROM notes WHERE id = ?", [noteId],(err,result)=>{
-            if(err){
-                res.send({message:"something went wrong"});
-                return;
-            }
-            res.send(result);
+      "INSERT INTO notes (title,body,owner) VALUES (?,?,?)",
+      [title, body, owner],
+      (err, result) => {
+        if (err) {
+          return res.status(400).json({ error: err });
         }
+        return res.status(201).json({ message: "Successfully created note!" });
+      }
     );
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
-router.post("/notes",(req,res)=>{
-    const login = req.body.login;
-
+router.put("/notes/:id", async (req, res) => {
+  const { login, id, title, body } = req.body;
+  console.log(login)
+  console.log(id)
+  console.log(title)
+  console.log(body)
+  try{
+    db.query(
+    "UPDATE notes SET title = ?, body = ? WHERE id = ? AND owner = ?",
+    [title,body,id,login],
+    (err, result, fields) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      console.log('note updated');
+      return res.status(204).json(result);
+    }
+    );
+  }catch(err){
+    return res.status(400).json({error:err});
+  }
 });
-router.put("/notes",(req,res)=>{
-    const login = req.body.login;
-
-});
-router.delete("/notes",(req,res)=>{
-    const login = req.body.login;
-
+router.delete("/notes/:id", (req, res) => {
+  const { login, id } = req.body;
+  try {
+    db.query(
+      "DELETE FROM notes WHERE id = ? AND owner = ?",
+      [id, login],
+      (err, result) => {
+        if (err) {
+          return res.status(400).json({ error: err });
+        }
+        console.log('note deleted');
+        return res.status(204).json({message: "successfully deleted"});
+      }
+    );
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
 module.exports = router;
--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -14,18 +14,41 @@ function App() {
   useEffect(() => {
     if (loggedId !== "") console.log("changed:", loggedId);
   }, [loggedId]);
-
+  function emptyCredentialsAlert() {
+    alert("Username or password can't be empty!");
+  }
+  function emptyNoteAlert() {
+    alert("Note title or note body can't be empty!");
+  }
   const registerUser = () => {
     if (loginRegister === "" || passwordRegister === "") {
-      console.log("empty");
-      return;
+      return emptyCredentialsAlert();
     }
-    Axios.post("http://localhost:3001/register", {
-      login: loginRegister,
-      password: passwordRegister,
-    })
-      .then((res) => {
-        if (res.data.err) {
+    try {
+      Axios.post("http://localhost:3001/register", {
+        login: loginRegister,
+        password: passwordRegister,
+      }).then((res) => {
+        if (res.data.error) {
+          console.log("something went wrong");
+          return;
+        }
+        console.log(res.data.message);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const loginUser = () => {
+    if (login === "" || password === "") {
+      return emptyCredentialsAlert();
+    }
+    try {
+      Axios.post("http://localhost:3001/login", {
+        login: login,
+        password: password,
+      }).then((res) => {
+        if (res.data.error) {
           console.log("something went wrong");
           return;
         }
@@ -33,57 +56,115 @@ function App() {
           console.log(res.data.message);
           return;
         }
-        console.log("successfully created");
-      })
-      .catch((err) => {
-        console.log(err);
+        setId(res.data[0].id);
       });
-  };
-  const loginUser = () => {
-    if (login === "" || password === "") {
-      console.log("empty");
-      return;
+    } catch (err) {
+      console.log(err);
     }
-    Axios.post("http://localhost:3001/login", {
-      login: login,
-      password: password,
-    }).then((res) => {
-      if (res.data.err) {
-        console.log("something went wrong");
-        return;
-      }
-      if (res.data.message) {
-        console.log(res.data.message);
-        return;
-      }
-      setId(res.data[0].id);
-    });
   };
   const getAllNotes = () => {
     Axios.get("http://localhost:3001/notes", {
-      params:{
-        login:loggedId
-      }
-    }).then((res)=>{
-      if(res.message){
-        console.log(res.message.data)
+      params: {
+        login: loggedId,
+      },
+    }).then((res) => {
+      if (res.message) {
+        console.log(res.message.data);
         return;
       }
       console.log(res.data);
     });
   };
-  const getNote = (noteId)=>{
-    Axios.get(`http://localhost:3001/notes/${noteId}`, {
-      params:{
-        id:noteId
-      }
-    }).then((res)=>{
-      if(res.message){
-        console.log(res.message.data)
-        return;
-      }
-      console.log(res.data);
-    });
+  const getNote = (noteId) => {
+    try {
+      Axios.get(`http://localhost:3001/notes/${noteId}`, {
+        params: {
+          nId: noteId,
+          login: loggedId,
+        },
+      })
+        .then((res) => {
+          if (res.status === 400) {
+            console.log(res.error);
+            return;
+          }
+          console.log(res.data[0]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const addNote = (noteTitle, noteBody) => {
+    if (noteTitle === "" || noteBody === "") {
+      return emptyNoteAlert();
+    }
+    try {
+      Axios.post("http://localhost:3001/notes", {
+        title: noteTitle,
+        body: noteBody,
+        owner: loggedId,
+      })
+        .then((res) => {
+          console.log(res.data.message);
+        })
+        .catch((err) => {
+          console.log(`note adding failed`);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const editNote = (noteId, noteTitle, noteBody) => {
+    if (noteTitle === "" || noteBody === "") {
+      return emptyNoteAlert();
+    }
+    try {
+      Axios.put(`http://localhost:3001/notes/${noteId}`, {
+        login: loggedId,
+        id: noteId,
+        title: noteTitle,
+        body: noteBody,
+      }).then((res) => {
+        if (res.status === 204) console.log("successfully updated note");
+      }).catch((error)=>{
+        console.log(error);
+      })
+    } catch (error) {
+      console.log(`error updating data: ${error}`);
+    }
+  };
+
+  const deleteNote = (noteId) => {
+    try {
+      Axios.delete(`http://localhost:3001/notes/${noteId}`, {
+        data: {
+          login: loggedId,
+          id: noteId,
+        },
+      })
+        .then((res) => {
+          if (res.status === 204) console.log("successfully deleted note");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(`error deleting data: ${error}`);
+    }
+  };
+
+  function takeNoteValues() {
+    const title = document.getElementById("tytul").value;
+    const val = document.getElementById("tresc").value;
+    addNote(title, val);
+  }
+  function editNoteValues(noteId) {
+    const title = document.getElementById("tytuledit").value;
+    const val = document.getElementById("trescedit").value;
+    editNote(noteId, title, val);
   }
   return (
     <div className="App">
@@ -126,8 +207,17 @@ function App() {
           <button onClick={loginUser}> login</button>
         </div>
       </div>
-      <button onClick={()=>getAllNotes()}>pobierz notki</button>
-      <button onClick={()=>getNote(4)}>notke</button>
+      <button onClick={() => getAllNotes()}>pobierz wszystkie notki</button>
+      <button onClick={() => getNote(5)}>pobierz 1 notke</button>
+      <input placeholder="tytul" id="tytul"></input>
+      <input placeholder="body" id="tresc"></input>
+      <button onClick={() => takeNoteValues()}>dodaj notke</button>
+      <button onClick={() => deleteNote(14)}>usun notatke</button>
+      <div>
+        <input placeholder="tytul" id="tytuledit"></input>
+        <input placeholder="body" id="trescedit"></input>
+        <button onClick={() => editNoteValues(9)}>edytuj notke</button>
+      </div>
     </div>
   );
 }
